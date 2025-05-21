441a7e88b7e1dc709eecbdc2e48e1598a9d47fb5
studente <manuel.scalisi@branchiana.edu.it>
2025-05-21 09:45:03 +0200

    aggiunto il main

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        }
+        Veicolo[] veicoli = new Veicolo[2];
+        veicoli[0] = new Automobile(5, "Fiat 500", 2020);
+        veicoli[1] = new Moto(998, "yamaha R1", 2021);
     }
+
+}
