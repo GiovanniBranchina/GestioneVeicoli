944f5f5e1abcd55ee1b7432c4cfb7b427842e2f7
studente <manuel.scalisi@branchiana.edu.it>
2025-05-21 09:45:44 +0200

    aggiunta la classe automobile

--- src/Automobile.java
+++ src/Automobile.java
@@ -1,2 +1,19 @@
-public class Automobile {
+public class Automobile extends Veicolo {
+    int porte;
+    public Automobile(int porte, int modello, int anno){
+        super (int modello, int anno);
+        this.porte = porte;
+    }
+
+    @Override
+    public void startMotore(){
+        return "Motore acceso: Vroom!";
+    }
+    public int getPorte(){
+        return porte;
+    }
+    public void setPorte(int porte){
+        this.porte = porte;
+    }
+
 }
