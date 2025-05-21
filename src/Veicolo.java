55300ae705c67a32e63d3a76ec5c52120267286c
studente <manuel.scalisi@branchiana.edu.it>
2025-05-21 09:00:09 +0200

    Initial Committ

---
+++ src/Veicolo.java
@@ -0,0 +1,12 @@
+public abstract class Veicolo {
+    private String getModello();
+    private int getAnno();
+    private void startMotore();
+
+    public Veicolo(String modello, int anno){
+        this.getModello();
+        this.getAnno();
+        this.startMotore();
+
+    }
+}
